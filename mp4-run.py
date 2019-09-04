11c29b491f4dda8bf5f45b707047913bc785afb5
David Schneider <d.a.schneider195@gmail.com>
2019-09-04 13:26:11 +0200

    added ff*.exe

--- mp4-run.py
+++
@@ -1,8 +0,0 @@
-# mp4-downloader
-
-import pytube
-#i = input("Geben Sie nun Ihren Link ein")
-url = 'https://www.youtube.com/watch?v=G-9hWUEjvIU'
-youtube = pytube.YouTube(url)
-video = youtube.stream.first()
-video.download()
