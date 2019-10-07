c00a035e8276c9a51aa15fa540973bba26a91e5d
David Schneider <d.a.schneider195@gmail.com>
2019-10-07 10:43:48 +0200

    implemented regex to validate youtube URLs. will be extended later or by user option. youtube-dl can handle some other sites as well

--- url_validator.py
+++ url_validator.py
@@ -1,18 +1,25 @@
 # import something for getting error codes
 import validators
+import re
 
 def check(link):
 
-    # must be a YouTube link
-    if ("www.youtube." not in link):
-        return False
-
-    # must not be a standard page
-    if ((link == "https://www.youtube.com/" ) or (link == "https://www.youtube.de/")):
-        return False
-
     # todo: check if is not working link, but yet valid
 
-    # return validation
+    # regular expression - must be with this beginning:
+    pattern = re.compile(r'^https://www.youtube.com/watch\?v=') # .youtube.com/watch?v=[a-z0-9]{11,}
+
+    #print(re.search(r'https://www\.youtube\.com/watch?v=\W{11,}', link))
+
+    matches = pattern.finditer(link)
+
+    for match in matches:
+        print(match)
+
     return validators.url(link)
+
+
+    # https://www.youtube.com/watch?v=ehs6vQTFPoA
+    # return validation
+    #return validators.url(link)
 
